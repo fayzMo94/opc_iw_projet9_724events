199bd33f8845e977869e8c990fbf9e08af0187fd
Xart94 <fayz_mohamed2@hotmail.fr>
2024-09-11 17:32:54 +0200

    slider debug

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
@@ -7,51 +7,72 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null); // Ref pour stocker le timeout ID
+
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
+  // by date descending : du plus grand au plus petit (date la plus récente dabord) donc ">" à la place de "<"
+
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+    timeoutRef.current = setTimeout(
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
+    // add -1 to "byDateDesc.length" to go from 0 to 2 in setIndex() (index 3 n'existe pas d'où le slide blanc)
   };
+
   useEffect(() => {
     nextCard();
   });
+
+  function setRadioBtn(idx) {
+    // Stoppe le timeout lorsqu'un bouton radio est cliqué
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    setIndex(idx); // Met à jour l'index selon le bouton radio cliqué
+  }
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
+              {/* <div>
+                {new Intl.DateTimeFormat("fr", { month: "long" }).format(
+                  new Date(event.date)
+                )}
+              </div> */}
+              {/* correction de la syntaxe pour afficher le mois correctement */}
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((_, radioIdx) => (
+            <input
+              key={`${radioIdx}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              // le radio "checked" doit changer selon le state de la const "index"
+              onChange={() => setRadioBtn(radioIdx)}
+              // add onChange to be able to click and change radio buttons
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -13,4 +13,5 @@ export const MONTHS = {
   12: "décembre",
 };
 
-export const getMonth = (date) => MONTHS[date.getMonth()];
+export const getMonth = (date) => MONTHS[date.getMonth() + 1];
+// correction affichage de la date en ajoutant +1 après "date.getMonth()"
